1ccb4cf4d72c87dad93ca33bad316dc54406c7f1
Pranav Byakod <pranav.byakod@gmail.com>
2021-06-24 16:30:44 -0700

    creating password length prompt

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // Creating a prompt to determine password length
+  passBool = false;
+  while(passBool == false) {
+    var passLength = prompt("Enter the length of the password. Note: The password length should be between 8 and 128 characters.");
+    // Checking if user did not follow specified instructions
+    if(passLength < 8 || passLength > 128) {
+    alert("The input values falls outside the specified range. Please enter a length between 8 and 128.")
+    }
+    else {
+      passBool = true;
+    }
+  }
+}
